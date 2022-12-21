2f6c56c7195dd6f961f2cfcd4a6738241dbfb12f
Tahoshin1 <tanrid008@gmail.com>
2022-12-21 23:46:38 +0600

    first commit

---
+++ Model/database/bus_booking.sql
@@ -0,0 +1,218 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 09, 2021 at 04:32 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bus_booking`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booked`
+--
+
+CREATE TABLE `booked` (
+  `id` int(30) NOT NULL,
+  `schedule_id` int(30) NOT NULL,
+  `ref_no` text NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `status` tinyint(1) DEFAULT 0 COMMENT '1=Paid, 0- Unpaid',
+  `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `booked`
+--
+
+INSERT INTO `booked` (`id`, `schedule_id`, `ref_no`, `name`, `qty`, `status`, `date_updated`) VALUES
+(1, 1, '202104291600', 'Shafin', 1, 1, '2021-12-09 16:00:00'),
+(2, 1, '202104290245', 'Khaled Ahmed', 2, 0, '2022-01-05 23:45:22');
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bus`
+--
+
+CREATE TABLE `bus` (
+  `id` int(30) NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `bus_number` varchar(50) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT '0 = inactive, 1 = active',
+  `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()) 
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `bus`
+--
+
+INSERT INTO `bus` (`id`, `name`, `bus_number`, `status`, `date_updated`) VALUES
+(1, 'Economy', '5001', 1, '2021-12-09 16:00:00'),
+(2, 'Business', '5002', 1, '2021-12-09 16:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `location`
+--
+
+CREATE TABLE `location` (
+  `id` int(30) NOT NULL,
+  `terminal_name` text NOT NULL,
+  `city` varchar(250) NOT NULL,
+  `state` varchar(250) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT '0= inactive , 1= active',
+  `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `location`
+--
+
+INSERT INTO `location` (`id`, `terminal_name`, `city`, `state`, `status`, `date_updated`) VALUES
+(1, 'Mohakhali', 'Dhaka', 'Bangladesh', 1, '2021-12-09 17:23:36'),
+(2, 'Gabtoli', 'Dhaka', 'Bangladesh', 1, '2021-12-09 17:24:58'),
+(3, 'Shaymoli', 'Dhaka', 'Bangladesh', 1, '2021-12-09 17:25:45');
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `schedule_list`
+--
+
+CREATE TABLE `schedule_list` (
+  `id` int(30) NOT NULL,
+  `bus_id` int(30) NOT NULL,
+  `from_location` int(30) NOT NULL,
+  `to_location` int(30) NOT NULL,
+  `departure_time` datetime NOT NULL,
+  `eta` datetime NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT 1,
+  `availability` int(11) NOT NULL,
+  `price` text NOT NULL,
+  `date_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `schedule_list`
+--
+
+INSERT INTO `schedule_list` (`id`, `bus_id`, `from_location`, `to_location`, `departure_time`, `eta`, `status`, `availability`, `price`, `date_updated`) VALUES
+(1, 1, 1, 2, '2022-01-08 16:00:00', '2021-12-16 17:00:00', 1, 30, '600', '2022-01-05 23:39:06'),
+(2, 2, 2, 3, '2022-01-09 02:45:00', '2021-12-16 03:45:00', 1, 30, '1200', '2022-01-05 23:39:31');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(30) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `user_type` tinyint(1) NOT NULL DEFAULT 1 COMMENT '1 = admin, 2= faculty , 3 = student',
+  `username` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT ' 0 = incative , 1 = active',
+  `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `user_type`, `username`, `password`, `status`, `date_updated`) VALUES
+(1, 'Shafin', 1, 'shafin99', 'shafin123', 1, '2021-12-17 22:58:13'),
+(2, 'Islam shafin', 1, 'islamshafin', 'islamshafin321', 1, '2021-12-17 16:55:28');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `booked`
+--
+ALTER TABLE `booked`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `bus`
+--
+ALTER TABLE `bus`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `location`
+--
+ALTER TABLE `location`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `schedule_list`
+--
+ALTER TABLE `schedule_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `booked`
+--
+ALTER TABLE `booked`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `bus`
+--
+ALTER TABLE `bus`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `location`
+--
+ALTER TABLE `location`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `schedule_list`
+--
+ALTER TABLE `schedule_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
